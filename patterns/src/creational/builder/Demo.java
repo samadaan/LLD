b493a65a6152a41f7a8c8ed137546cfc0a04d959
Sarthak <sarthakmadaan5@gmail.com>
2024-04-30 01:05:22 +0530

    random

--- patterns/src/creational/builder/Demo.java
+++ patterns/src/creational/builder/Demo.java
@@ -1,2 +1,9 @@
-package creational.builder;public class Demo {
+package creational.builder;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        House house=new House.HouseBuilder().setAddress("Okayayy").setDoors(4).setFurnished(true).build();
+        System.out.println(house);
+    }
 }
--- patterns/src/creational/builder/House.java
+++ patterns/src/creational/builder/House.java
@@ -1,2 +1,45 @@
-package creational.builder;public class House {
+package creational.builder;
+
+public class House {
+    private int doors;
+    private String address;
+    private boolean furnished;
+
+    private House(){
+
+    }
+
+    private House(HouseBuilder houseBuilder){
+        this.doors=houseBuilder.doors;
+        this.furnished=houseBuilder.furnished;
+        this.address=houseBuilder.address;
+    }
+
+    static class HouseBuilder{
+
+        private int doors;
+        private String address;
+        private boolean furnished;
+
+        public HouseBuilder setDoors(int doors){
+            this.doors=doors;
+            return this;
+        }
+        public HouseBuilder setAddress(String address){
+            this.address=address;
+            return this;
+        }
+        public HouseBuilder setFurnished(boolean furnished){
+            this.furnished=furnished;
+            return this;
+        }
+        public House build(){
+            return new House(this);
+        }
+    }
+
+    public String toString(){
+        return " "+this.address+" "+this.furnished+" "+this.doors;
+    }
+
 }
